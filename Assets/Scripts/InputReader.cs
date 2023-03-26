753ed0a9b1238311c8fd0239a79726743286f702
YanaManhusheva <yanamanhusheva@gmail.com>
2023-03-26 13:06:45 +0200

    Added vertical movement

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -7,15 +7,20 @@ namespace Assets.Scripts
     public class InputReader : MonoBehaviour
     {
         [SerializeField] private PlayerEntity _playerEntity;
-        private float _direction;
+
+        private float _horizontalDirection;
+        private float _verticalDirection;
+
         private void Update()
         {
-            _direction = Input.GetAxisRaw("Horizontal");
+            _horizontalDirection = Input.GetAxisRaw("Horizontal");
+            _verticalDirection = Input.GetAxisRaw("Vertical");
             
         }
         private void FixedUpdate()
         {
-            _playerEntity.MoveHorizontally(_direction);
+            _playerEntity.MoveHorizontally(_horizontalDirection);
+            _playerEntity.MoveVertically(_verticalDirection);
         }
     }
 }
--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -8,22 +8,57 @@ namespace Player
 
     public class PlayerEntity : MonoBehaviour
     {
+        [Header("HorizintalMovement")]
         [SerializeField] private float _horizontalSpeed;
+        [SerializeField] private bool _faceRight;
+
+
+        [Header("VerticalMovement")]
+        [SerializeField] private float _verticalSpeed;
+        [SerializeField] private float _minSize;
+        [SerializeField] private float _maxSize;
+        [SerializeField] private float _minVericalPosition;
+        [SerializeField] private float _maxVericalPosition;
+
+
         private Rigidbody2D _rigidbody;
+
+        private float _sizeModificator;
+
 
         // Start is called before the first frame update
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+
+            float positionDifference = _maxVericalPosition - _minVericalPosition;
         }
 
         public void MoveHorizontally(float direction)
         {
+            SetDirection(direction);
             Vector2 velocity = _rigidbody.velocity;
             velocity.x = direction * _horizontalSpeed;
             _rigidbody.velocity = velocity;
         }
+        public void MoveVertically(float direction)
+        {
+            Vector2 velocity = _rigidbody.velocity;
+            velocity.y = direction * _verticalSpeed;
+            _rigidbody.velocity = velocity;
+        }
 
-      
+      private void SetDirection(float direction)
+        {
+            if (_faceRight && direction<0 || !_faceRight && direction > 0)
+            {
+                Flip();
+            }
+        }
+        private void Flip()
+        {
+            transform.Rotate(0, 180, 0);
+            _faceRight = !_faceRight;
+        }
     }
 }
