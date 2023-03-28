bfcd256e18619bbda208bd6b567d851ea3747e2f
YanaManhusheva <yanamanhusheva@gmail.com>
2023-03-28 20:32:19 +0200

    Added joystick with a jump and attack buttons

---
+++ Assets/Scripts/Player/PlayerBrain.cs
@@ -0,0 +1,58 @@
+﻿using Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerBrain
+    {
+        private readonly PlayerEntity _playerEntity;
+        private readonly List<IEntityInputSource> _inputSources;
+        public PlayerBrain(PlayerEntity playerEntity, List<IEntityInputSource> inputSources)
+        {
+            _playerEntity = playerEntity;
+            _inputSources = inputSources;
+        }
+        public void OnFixedUpdate()
+        {
+            _playerEntity.MoveHorizontally(GetHorizontalDirection());
+            _playerEntity.MoveVertically(GetVerticalDirection());
+            if (IsJump)
+                _playerEntity.Jump();
+            if (IsAttack)
+                _playerEntity.StartAttack();
+
+            foreach (var inputSource in _inputSources)
+                inputSource.ResetOneTimeAction();
+        }
+        private float GetHorizontalDirection()
+        {
+            foreach (var inputSource in _inputSources)
+            {
+                if (inputSource.HorizontalDirection == 0)
+                    continue;
+
+                return inputSource.HorizontalDirection;
+            }
+
+            return 0;
+        }
+        private float GetVerticalDirection()
+        {
+            foreach (var inputSource in _inputSources)
+            {
+                if (inputSource.VerticalDirection == 0)
+                    continue;
+
+                return inputSource.VerticalDirection;
+            }
+
+            return 0;
+        }
+        private bool IsJump => _inputSources.Any(source => source.Jump);
+        private bool IsAttack => _inputSources.Any(source => source.Attack); 
+    }
+}
